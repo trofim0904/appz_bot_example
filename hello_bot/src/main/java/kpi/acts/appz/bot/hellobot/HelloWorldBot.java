9be3f4e85bafa1a329399409a126a922b7f67294
Dima Trofimchuk <dimatrofimchuk@ukr.net>
2020-03-10 15:19:40 +0200

    Something

--- hello_bot/src/main/java/kpi/acts/appz/bot/hellobot/HelloWorldBot.java
+++ hello_bot/src/main/java/kpi/acts/appz/bot/hellobot/HelloWorldBot.java
@@ -26,6 +26,6 @@ public final class HelloWorldBot extends Bot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        sendTextMessage(update.getMessage(), "Hello world!");
+        sendTextMessage(update.getMessage(), "HOHOHOHHOHOHOHO");
     }
 }
